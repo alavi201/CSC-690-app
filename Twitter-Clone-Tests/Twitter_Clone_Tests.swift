c165ab9fb30f954c8a6dee2a2717d00f4faac5b7
alialavi <ali.alavi201@gmail.com>
2019-05-16 11:46:05 -0700

    Added test case for valid url host and path

---
+++ Twitter-Clone-Tests/Twitter_Clone_Tests.swift
@@ -0,0 +1,33 @@
+//
+//  Twitter_Clone_Tests.swift
+//  Twitter-Clone-Tests
+//
+//  Created by Ali Alavi on 5/16/19.
+//  Copyright © 2019 SFSU. All rights reserved.
+//
+
+import XCTest
+
+class Twitter_Clone_Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
