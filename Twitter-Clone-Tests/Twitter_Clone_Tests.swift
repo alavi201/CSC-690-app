18ac5fa79a23013c3f17772185b11d8b6f574e22
alialavi <ali.alavi201@gmail.com>
2019-05-16 16:02:36 -0700

    Testing Server 200 Response

--- Twitter-Clone-Tests/Twitter_Clone_Tests.swift
+++ Twitter-Clone-Tests/Twitter_Clone_Tests.swift
@@ -10,24 +10,40 @@ import XCTest
 
 class Twitter_Clone_Tests: XCTestCase {
 
+    var sessionUnderTest: URLSession!
+    
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+        sessionUnderTest = URLSession(configuration: URLSessionConfiguration.default)
     }
-
+    
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sessionUnderTest = nil
+        super.tearDown()
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testValidCallToServerGetsHTTPStatusCode200() {
+        // given
+        let url = URL(string: "http://127.0.0.1:8081")
+        // 1
+        let promise = expectation(description: "Status code: 200")
+        
+        // when
+        let dataTask = sessionUnderTest.dataTask(with: url!) { data, response, error in
+            // then
+            if let error = error {
+                XCTFail("Error: \(error.localizedDescription)")
+                return
+            } else if let statusCode = (response as? HTTPURLResponse)?.statusCode {
+                if statusCode == 200 {
+                    // 2
+                    promise.fulfill()
+                } else {
+                    XCTFail("Status code: \(statusCode)")
+                }
+            }
         }
+        dataTask.resume()
+        // 3
+        waitForExpectations(timeout: 5, handler: nil)
     }
-
 }
