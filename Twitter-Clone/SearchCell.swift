42ae086b8ac7854f57aaed99828aa62b5c5f9d94
Ali Alavi <ali.alavi201@gmail.com>
2019-05-15 21:15:54 -0700

    Create SearchCell.swift

---
+++ Twitter-Clone/SearchCell.swift
@@ -0,0 +1,25 @@
+//
+//  SearchCell.swift
+//  Twitter-Clone
+//
+//  Created by Vipul Karanjkar on 5/11/19.
+//  Copyright © 2019 SFSU. All rights reserved.
+//
+
+import UIKit
+
+class SearchCell: UITableViewCell {
+    
+    @IBOutlet weak var cellText: UILabel!
+    @IBOutlet weak var cellButton: UIButton!
+    @IBOutlet weak var cellImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
